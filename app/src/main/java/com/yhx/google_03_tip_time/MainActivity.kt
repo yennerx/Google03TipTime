793bfa6afdd6329579f006e05bd049c73123c853
Yenner Hernández <yenner.hernandez@hotmail.com>
2021-04-13 11:06:39 -0600

    Se agregó un ScrollView para desplazarse en la app con el ce en modo Horizontal (Paisaje). Se agregó la función de ocultar teclado virtual al presionar la tecla enter del teclado en la pantalla. Se modificaron los íconos de la app para que cambien de color segun el tema de la app (normal/oscuro).

--- app/src/main/java/com/yhx/google_03_tip_time/MainActivity.kt
+++ app/src/main/java/com/yhx/google_03_tip_time/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.yhx.google_03_tip_time
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.yhx.google_03_tip_time.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -19,6 +23,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)/**Se agrega código para enlace de vista. (Configure la vista de contenido de la actividad. En lugar de pasar el ID de recurso del diseño, R.layout.activity_main, especifica la raíz de la jerarquía de puntos de vista en su aplicación, binding.root.)*/
 
         binding.calculateButton.setOnClickListener{calculateTip()}
+
+        /**Ocultar el teclado virtual al presionar enter. Parte 2/2.
+         * Call the setOnKeyListener() method on the costOfServiceEditText and pass in an OnKeyListener. This is similar to how you set a click listener on the calculate button in the app with binding.calculateButton.setOnClickListener { calculateTip() }.
+         * The code for setting a key listener on a view is a little more complex, but the general idea is that OnKeyListener has an onKey() method that gets triggered when a key press happens. The onKey() method takes in 3 input arguments: the view, the code for the key that was pressed, and a key event (which you won't use, so you can call it "_"). When the onKey() method is called, you should call your handleKeyEvent() method and pass along the view and key code arguments. The syntax for writing this out is: view, keyCode, _ -> handleKeyEvent(view, keyCode). This is actually called a lambda expression, but you will learn more about lambdas in a later unit.*/
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip(){
@@ -51,5 +63,20 @@ class MainActivity : AppCompatActivity() {
         NumberFormat.getCurrencyInstance()
         val formatedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_mount, formatedTip)
+    }
+
+/**Método para ocultar el teclado virtual al presionar enter. Parte 1/2.
+ * The handleKeyEvent() is a private helper function that hides the onscreen keyboard if the keyCode input parameter is equal to KeyEvent.KEYCODE_ENTER.
+ * The InputMethodManager controls if a soft keyboard is shown, hidden, and allows the user to choose which soft keyboard is displayed.
+ * The method returns true if the key event was handled, and returns false otherwise. */
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
